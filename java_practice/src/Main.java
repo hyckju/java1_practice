7da3532383cb2e86a0a64fccfa6bc17a2234f205
yhj <hyckjoo0125@gmail.com>
2025-03-27 14:51:59 +0900

    형변

---
+++ java_practice/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]) {
+        String name = "양혁주";
+        int age = 23;
+
+        System.out.println(name + "님은 " +age+ "살 입니다.");
+    }
+
+}
+
